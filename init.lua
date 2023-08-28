2f2e82e220c97c0d37dfe26422e1213d19d3a9e7
Rene Beneke <rbeneke@tecracer.de>
2023-08-28 17:20:42 +0200

    add vim-surround, autopairs, ctrl-p for fuzzy find

--- init.lua
+++ init.lua
@@ -38,6 +38,7 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   'vim-scripts/ReplaceWithRegister',
+  'tpope/vim-surround',
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -51,6 +52,11 @@ require('lazy').setup({
     dependencies = {
       'kana/vim-textobj-user',
     },
+  },
+  {
+  'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {}
   },
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -286,6 +292,8 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+-- secondary mapping
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
